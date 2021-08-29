6183457978a7c5906bcf5f00bffeac48300fe5a3
Numichi <csdonat1@gmail.com>
2021-08-29 12:22:14 +0200

    first commit

---
+++ src/main/java/hu/flowacademy/spring/FlowAcademyLambdaSpringApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowAcademyLambdaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowAcademyLambdaSpringApplication.class, args);
+    }
+
+}
